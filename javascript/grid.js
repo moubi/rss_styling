4c5ee27840d3a26f5b324ad5602776c05c65acc0
Miroslav Nikolov <miroslavnikolov@ymail.com>
2015-05-21 13:59:54 +0300

    Make default grid dynamic

--- javascript/grid.js
+++ javascript/grid.js
@@ -14,23 +14,21 @@
       var padding = 380/this.length + 'px',
         fontSize = 555/this.length - (15 - this.length) + '%';
 
-      Grid.$container.css('font-size', fontSize);
-      Grid.$container.children().css({
-        'padding-top': padding,
-        'padding-bottom': padding
-      });
-
     } else if (this.type == 'special') {
       var padding = 450/this.length + 'px',
         fontSize = 15*(6 + 15/this.length) + '%';
-
-      Grid.$container.css('font-size', fontSize);
-      Grid.$container.children().css({
-        'padding-top': padding,
-        'padding-bottom': padding
-      });
     }
+
+    _css(fontSize, padding);
   };
+
+  function _css(fontSize, padding) {
+    Grid.$container.css('font-size', fontSize);
+    Grid.$container.children().css({
+      'padding-top': padding,
+      'padding-bottom': padding
+    });
+  }
 
   window.Grid = Grid;
 })();

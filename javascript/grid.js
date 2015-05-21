1092902958540917761b284a942784507497c2b6
Miroslav Nikolov <miroslavnikolov@ymail.com>
2015-05-21 16:44:47 +0300

    Set dynamic grid layout for horizontal view

--- javascript/grid.js
+++ javascript/grid.js
@@ -10,24 +10,42 @@
   }
 
   Grid.prototype.scale = function() {
+    var padding, fontSize, width;
+
     if (this.type == 'list') {
-      var padding = 380/this.length + 'px',
-        fontSize = 555/this.length - (15 - this.length) + '%';
+      padding = 380/this.length,
+      fontSize = 555/this.length - (15 - this.length) + '%';
 
     } else if (this.type == 'special') {
-      var padding = 450/this.length + 'px',
-        fontSize = 15*(6 + 15/this.length) + '%';
+      padding = 450/this.length,
+      fontSize = 15*(6 + 15/this.length) + '%';
+
+    } else {
+      padding = 510/this.length,
+      fontSize = 1500/this.length - (105 - 7*this.length) + '%';
+
+      if (this.length <= 4 && this.length > 1) {
+        width = '43%';
+        padding = padding - 30 * (5 - this.length);
+        fontSize = parseInt(fontSize) - (100 * (5 - this.length)) + '%';
+
+      } else {
+        width = '100%';
+        padding = 180;
+        fontSize = '350%';
+      }
     }
 
-    _css(fontSize, padding);
+    _css(fontSize, padding, width);
   };
 
-  function _css(fontSize, padding) {
+  function _css(fontSize, padding, width) {
     Grid.$container.css('font-size', fontSize);
     Grid.$container.children().css({
       'padding-top': padding,
       'padding-bottom': padding
     });
+    width && Grid.$container.children().css('width', width);
   }
 
   window.Grid = Grid;
